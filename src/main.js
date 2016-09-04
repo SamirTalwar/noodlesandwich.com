47a16dd8187916adf902f34bbe09ef517e89cd3d
Samir Talwar <samir@noodlesandwich.com>
2016-09-04 18:46:26 +0100

    Use SASS for the site's CSS, and serve it.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const koa = {
   route: require('koa-route')
 }
 const pug = require('pug')
+const sass = require('node-sass')
 
 const env = name => {
   const value = process.env[name]
@@ -17,28 +18,35 @@ const env = name => {
 
 const port = parseInt(env('PORT'), 10)
 
-const readFile = function (...args) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(...args, (error, data) => {
+const denodeify = func => (...args) =>
+  new Promise((resolve, reject) => {
+    func(...args, (error, value) => {
       if (error) {
         reject(error)
       } else {
-        resolve(data)
+        resolve(value)
       }
     })
   })
-}
+
+const readFile = denodeify(fs.readFile)
+const renderSass = denodeify(sass.render)
 
 const pages = {
   home: function*() {
     const template = yield readFile('views/home.pug')
-    return pug.compile(template)()
+    return pug.compile(template, {filename: 'views/home.pug', pretty: true})()
   }
 }
 
 const app = koa.app()
 app.use(koa.route.get('/', function*() {
+  this.type = 'text/html'
   this.body = yield pages.home()
+}))
+app.use(koa.route.get('/site.css', function*() {
+  this.type = 'text/css'
+  this.body = (yield renderSass({file: 'src/site.scss'})).css
 }))
 
 app.listen(port)
