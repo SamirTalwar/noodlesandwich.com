5efaec29689d8ab27e999f726cc0e8b26480e9e7
Samir Talwar <samir@noodlesandwich.com>
2019-05-25 13:57:02 +0100

    Upgrade to Terraform v0.12.

--- main.tf
+++ main.tf
@@ -16,10 +16,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-provider "cloudflare" {}
+provider "cloudflare" {
+}
 
 resource "aws_s3_bucket" "site" {
-  bucket = "${local.domain}"
+  bucket = local.domain
   acl    = "public-read"
 
   website {
@@ -42,11 +43,11 @@ resource "aws_s3_bucket" "assets" {
 resource "aws_cloudfront_distribution" "site_distribution" {
   enabled             = true
   default_root_object = "index.html"
-  aliases             = ["${local.domain}", "www.${local.domain}"]
+  aliases             = [local.domain, "www.${local.domain}"]
 
   origin {
     origin_id   = "S3-Website-${aws_s3_bucket.site.id}"
-    domain_name = "${aws_s3_bucket.site.website_endpoint}"
+    domain_name = aws_s3_bucket.site.website_endpoint
 
     custom_origin_config {
       http_port              = 80
@@ -92,7 +93,7 @@ resource "aws_cloudfront_distribution" "assets_distribution" {
 
   origin {
     origin_id   = "S3-${aws_s3_bucket.assets.id}"
-    domain_name = "${aws_s3_bucket.assets.bucket_regional_domain_name}"
+    domain_name = aws_s3_bucket.assets.bucket_regional_domain_name
   }
 
   default_cache_behavior {
@@ -126,69 +127,69 @@ resource "aws_cloudfront_distribution" "assets_distribution" {
 }
 
 resource "cloudflare_record" "root" {
-  domain  = "${local.domain}"
+  domain  = local.domain
   name    = "@"
   type    = "CNAME"
-  value   = "${aws_cloudfront_distribution.site_distribution.0.domain_name}"
+  value   = aws_cloudfront_distribution.site_distribution.domain_name
   proxied = true
 }
 
 resource "cloudflare_record" "www" {
-  domain  = "${local.domain}"
+  domain  = local.domain
   name    = "www"
   type    = "CNAME"
-  value   = "${local.domain}"
+  value   = local.domain
   proxied = true
 }
 
 resource "cloudflare_record" "assets" {
-  domain  = "${local.domain}"
+  domain  = local.domain
   name    = "assets"
   type    = "CNAME"
-  value   = "${aws_cloudfront_distribution.assets_distribution.0.domain_name}"
+  value   = aws_cloudfront_distribution.assets_distribution.domain_name
   proxied = true
 }
 
 resource "cloudflare_record" "alternative_root" {
-  domain  = "${local.alternative_domain}"
+  domain  = local.alternative_domain
   name    = "@"
   type    = "CNAME"
-  value   = "${local.domain}"
+  value   = local.domain
   proxied = true
 }
 
 resource "cloudflare_record" "alternative_www" {
-  domain  = "${local.alternative_domain}"
+  domain  = local.alternative_domain
   name    = "www"
   type    = "CNAME"
-  value   = "${local.domain}"
+  value   = local.domain
   proxied = true
 }
 
 resource "cloudflare_record" "alternative_talks" {
-  domain  = "${local.alternative_domain}"
+  domain  = local.alternative_domain
   name    = "talks"
   type    = "CNAME"
-  value   = "${local.domain}"
+  value   = local.domain
   proxied = true
 }
 
 resource "cloudflare_page_rule" "always_use_https" {
-  zone     = "${local.domain}"
+  zone     = local.domain
   target   = "http://*${local.domain}/*"
   priority = 1
 
-  actions = {
+  actions {
     always_use_https = true
   }
 }
 
 resource "cloudflare_page_rule" "redirect_www" {
-  zone     = "${local.domain}"
+  zone     = local.domain
   target   = "www.${local.domain}/*"
   priority = 2
 
-  actions = {
+  actions {
     forwarding_url {
       url         = "https://${local.domain}/$1"
       status_code = 301
@@ -197,11 +198,11 @@ resource "cloudflare_page_rule" "redirect_www" {
 }
 
 resource "cloudflare_page_rule" "redirect_alternative_root" {
-  zone     = "${local.alternative_domain}"
+  zone     = local.alternative_domain
   target   = "${local.alternative_domain}/*"
   priority = 1
 
-  actions = {
+  actions {
     forwarding_url {
       url         = "https://${local.domain}/$1"
       status_code = 301
@@ -210,11 +211,11 @@ resource "cloudflare_page_rule" "redirect_alternative_root" {
 }
 
 resource "cloudflare_page_rule" "redirect_alternative_www" {
-  zone     = "${local.alternative_domain}"
+  zone     = local.alternative_domain
   target   = "www.${local.alternative_domain}/*"
   priority = 2
 
-  actions = {
+  actions {
     forwarding_url {
       url         = "https://${local.domain}/$1"
       status_code = 301
@@ -223,11 +224,11 @@ resource "cloudflare_page_rule" "redirect_alternative_www" {
 }
 
 resource "cloudflare_page_rule" "redirect_alternative_talks" {
-  zone     = "${local.alternative_domain}"
+  zone     = local.alternative_domain
   target   = "talks.${local.alternative_domain}/*"
   priority = 3
 
-  actions = {
+  actions {
     forwarding_url {
       url         = "https://${local.domain}/talks/$1"
       status_code = 301
@@ -236,62 +237,62 @@ resource "cloudflare_page_rule" "redirect_alternative_talks" {
 }
 
 resource "cloudflare_record" "mail" {
-  domain = "${local.domain}"
+  domain = local.domain
   name   = "mail"
   type   = "CNAME"
   value  = "ghs.googlehosted.com"
 }
 
 resource "cloudflare_record" "mx_0" {
-  domain   = "${local.domain}"
-  name     = "${local.domain}"
+  domain   = local.domain
+  name     = local.domain
   type     = "MX"
   value    = "aspmx.l.google.com"
   priority = 1
 }
 
 resource "cloudflare_record" "mx_1" {
-  domain   = "${local.domain}"
-  name     = "${local.domain}"
+  domain   = local.domain
+  name     = local.domain
   type     = "MX"
   value    = "alt1.aspmx.l.google.com"
   priority = 5
 }
 
 resource "cloudflare_record" "mx_2" {
-  domain   = "${local.domain}"
-  name     = "${local.domain}"
+  domain   = local.domain
+  name     = local.domain
   type     = "MX"
   value    = "alt2.aspmx.l.google.com"
   priority = 5
 }
 
 resource "cloudflare_record" "mx_3" {
-  domain   = "${local.domain}"
-  name     = "${local.domain}"
+  domain   = local.domain
+  name     = local.domain
   type     = "MX"
   value    = "alt3.aspmx.l.google.com"
   priority = 10
 }
 
 resource "cloudflare_record" "mx_4" {
-  domain   = "${local.domain}"
-  name     = "${local.domain}"
+  domain   = local.domain
+  name     = local.domain
   type     = "MX"
   value    = "alt4.aspmx.l.google.com"
   priority = 10
 }
 
 resource "cloudflare_record" "keybase_site_verification" {
-  domain = "${local.domain}"
-  name   = "${local.domain}"
+  domain = local.domain
+  name   = local.domain
   type   = "TXT"
   value  = "keybase-site-verification=YPF-r-na8c2rGHEe5DxOI0xzGC1sSqr8743dqV4iF2o"
 }
 
 resource "cloudflare_record" "alternative_keybase_site_verification" {
-  domain = "${local.alternative_domain}"
-  name   = "${local.alternative_domain}"
+  domain = local.alternative_domain
+  name   = local.alternative_domain
   type   = "TXT"
   value  = "keybase-site-verification=mekOQ5MzFzpNa9ql62LM0IfNgZhcbpW7VSsj5mGOCxk"
 }
