1fd4b239ef641294275aaf22d0c23d7dc5cd103d
Samir Talwar <samir@functional.computer>
2024-12-22 23:48:28 +0100

    Always make HTTP calls to S3.
    The S3 bucket doesn't support HTTPS. Maybe it did in the past, but it
    definitely doesn't now.

--- main.tf
+++ main.tf
@@ -63,8 +63,8 @@ resource "aws_cloudfront_distribution" "site_distribution" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "match-viewer"
-      origin_ssl_protocols   = ["TLSv1.2"]
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
 
